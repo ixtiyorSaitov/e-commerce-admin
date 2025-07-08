c42254f6b64db898b9bcffa00133263b1a91b15a
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-07-08 23:12:33 +0500

    refactor: update supabase client to use environment variables for URL and anon key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = process.env.SUPABASE_URL;
-export const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3Y3l1bmtob3Zncm93Z3FlbG11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExODI5OTEsImV4cCI6MjA2Njc1ODk5MX0.YNLpxFoEaU-f6aLNic5bTwDZEiJFRhd5tetDYCT2jVY"; // yuqoridagi `anon public` key
+export const supabaseUrl = process.env.SUPABASE_URL!;
+export const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!; // yuqoridagi `anon public` key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
