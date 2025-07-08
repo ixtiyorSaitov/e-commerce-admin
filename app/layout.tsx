b6c6f3aeca59c3ee3d66cc2d308f84552af830cb
Ixtiyor <ixtiyorsaitov21@gmail.com>
2025-07-08 23:17:41 +0500

    refactor: update supabase client to use hardcoded URL and anon key

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,23 +24,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <NextTopLoader
-          color="#2299DD"
-          initialPosition={0.08}
-          crawlSpeed={200}
-          height={3}
-          crawl={true}
-          showSpinner={true}
-          easing={"ease"}
-          speed={200}
-          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
-        />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
+          <NextTopLoader showSpinner={false} />
           {children}
           {/* <AuthProvider></AuthProvider> */}
           <Toaster />
